2309696fd192816825c5dfafc67964adaac85cff
Alexis DUCHET <alexis.duchet@epita.fr>
2021-03-25 19:23:45 +0000

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -24,9 +24,26 @@ namespace BookController
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllers();
-        }
+		{
+			services.AddControllers();
+			services.AddDbContext<Context>(opt =>
+            {
+                opt.UseMySql(Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+
+			services.AddSwaggerGen(swagger =>  
+            {  
+                //This is to generate the Default UI of Swagger Documentation  
+                swagger.SwaggerDoc("v1", new OpenApiInfo  
+                {   
+                    Version= "v1",   
+                    Title = "Dorset College API",  
+                    Description="ASP.NET Core 3.1 Web API Documentaion" 
+                });
+            });
+
+		}
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -34,6 +51,8 @@ namespace BookController
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+				app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
 
             app.UseHttpsRedirection();
