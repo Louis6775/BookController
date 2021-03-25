75444ab4bc03aabd7589e00d1e8ef53f268d88e9
Alexis DUCHET <alexis.duchet@epita.fr>
2021-03-25 20:04:50 +0000

    Update BookDTO.cs

--- DTO/BookDTO.cs
+++ DTO/BookDTO.cs
@@ -6,6 +6,7 @@ namespace DTO
     {
         public decimal Book_price { get; set; }
         public string ISBN { get; set; }
+        public int Book_id { get; set; }
         public string Book_name { get; set; }
         public string Book_description { get; set; }
     }
